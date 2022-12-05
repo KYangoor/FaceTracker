082530c9ad95f5d449d8f15d03b666fc5475a62f
Edilberto <KYangoor@163.com>
2022-12-05 09:04:46 +0800

    Some improve

--- base/main.py
+++ base/main.py
@@ -2,34 +2,59 @@ import torch
 import cv2 as cv
 
 
-def box(input, data):
-    if len(data) >= 1:
-        input = cv.rectangle(input,
-                             (int(data['xcenter'][0] - data['width'][0] / 2),
-                              int(data['ycenter'][0] + data['height'][0] / 2)),
-                             (int(data['xcenter'][0] + data['width'][0] / 2),
-                              int(data['ycenter'][0] - data['height'][0] / 2)),
-                             (255, 255, 255), 2)
-        print('tgt_pos: ' + str(int(data['xcenter'][0])) + ',' + str(int(data['ycenter'][0])))
-        return input
+def box(im, pos):
+    if len(pos) >= 1:
+        im = cv.rectangle(im,
+                          (int(pos['xcenter'][0] - pos['width'][0] / 2),
+                              int(pos['ycenter'][0] + pos['height'][0] / 2)),
+                          (int(pos['xcenter'][0] + pos['width'][0] / 2),
+                              int(pos['ycenter'][0] - pos['height'][0] / 2)),
+                          (255, 255, 255), 2)
+        print('tgt_pos: ' + str(int(pos['xcenter'][0])) + ',' + str(int(pos['ycenter'][0])))
+        return im
     else:
-        return input
+        return im
 
 
-if __name__ == '__main__':
+def from_stream(addr):
     model = torch.hub.load('../modules/yolo', 'custom', path='../model/best.pt', source='local')
-
-    cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture(addr)
     while True:
         status, img = cap.read()
         if status:
             results = model(img)
             data = results.pandas().xywh[0]
             img = box(img, data)
-            out = cv.putText(img, 'press \'q\' to exit \'s\' to save face\n',
+            out = cv.putText(img, 'press \'q\' to exit \'s\' to save face',
                              (0, 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0))
             cv.imshow('1', out)
             if cv.waitKey(1) == ord('s'):
-                crops = results.crop(save=True)
+                results.crop(save=True)
             if cv.waitKey(1) == ord('q'):
                 break
+
+
+def from_localimg(addr):
+    model = torch.hub.load('../modules/yolo', 'custom', path='../model/best.pt', source='local')
+    img = cv.imread(addr)
+    results = model(img)
+    data = results.pandas().xywh[0]
+    img = box(img, data)
+    results.show()
+
+
+if __name__ == '__main__':
+    src = input('choose your img source\n'
+                '1.camera\n'
+                '2.local img\n'
+                '3.local video\n\n'
+                'input num: ')
+    if src == str(1):
+        from_stream(0)
+    elif src == str(2):
+        iaddr = input('input the addr of img: ')
+        from_localimg(iaddr)
+    elif src == str(3):
+        vaddr = input('input the addr of video: ')
+        from_stream(0)
+    # print(src)
